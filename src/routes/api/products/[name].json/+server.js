2f3a4d13d6cebeb1eb7b2b5ec59464193c7834c2
Kevin <50965376+tgodkev@users.noreply.github.com>
2022-11-05 17:15:43 -0400

    amazon scraping

--- src/routes/api/products/[name].json/+server.js
+++ src/routes/api/products/[name].json/+server.js
@@ -1,17 +1,34 @@
 import { json } from "@sveltejs/kit";
+import cheerio from 'cheerio';
 
 export async function GET(params) {
-    
+
+ const res = await fetch(`https://www.amazon.com/s?k=${params.params.name}`);
+
+//  https://www.amazon.com/s?k=cereal
+// https://www.walmart.com/search/?cat_id=0&query=beans
+ const dom = cheerio.load(await res.text());
+    const products = [];
+    const productList = dom(`div[data-index]`)
+
+    for (let i = 0; i < productList.length; i++) {
+        const product = productList[i];
+        const title = dom(product).find(`h2.a-size-mini`).text();
+        const price = dom(product).find(`span.a-price-whole`).text();
+        const image = dom(product).find(`img.s-image`).attr('src');
+
+        
+
+        products.push({
+            title,
+            price,
+            image
+           
+        })
+    }
 
 
-    const productName = params.name;
-
-    console.log(productName, 'from the api route');
-    return json({ name: productName });
-
-    // return new Response(JSON.stringify(productName))
-// this is where we will scrape the data from the website.
 
   
-//   return new Response(JSON.stringify(html) );
+  return new Response(JSON.stringify(products) );
 }
--- src/routes/product/[name]/+page.svelte
+++ src/routes/product/[name]/+page.svelte
@@ -1,7 +1,24 @@
 <script>
   export let data;
+
+  console.log(data.body);
+  // console.log(data.body.products);
 </script>
 
 <div>
-  {@html data}
+  <a href="/">
+    <button class="btn">Home</button>
+  </a>
+</div>
+
+<div class="grid grid-cols-3">
+  {#each data.body as product}
+    {#if product.title}
+      <div class="w-1/2">
+        <h2 class="truncate">{product.title}</h2>
+        <img src={product.image} alt="" />
+        <p>${product.price}</p>
+      </div>
+    {/if}
+  {/each}
 </div>
